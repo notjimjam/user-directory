2ea59cf50de6a22d18d7f2537d4451ef6bec6212
Jamie Smith <thejamiemsmith@gmail.com>
2022-04-26 18:35:09 -0700

    created a form for new users

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import './Card.css'
 import data from '../data'
+import Form from './Form'
 
 function Card(props) {
     const users = props.data.slice(0, data.length)
@@ -26,6 +27,7 @@ function Card(props) {
 
   return (
     <div className='card'>
+        {/* <Form /> */}
         <div>
             <h1>{index + 1}/{users.length}</h1>
             <h2 className='card-name'>{user.name.first} {user.name.last}</h2>
@@ -33,15 +35,18 @@ function Card(props) {
 
         <br />
         <div>
-            <p><span className='card-titles'>From:</span> {user.city}</p>
+            <p><span className='card-titles'>From:</span> {user.city}, {user.country}</p>
             <p><span className='card-titles'>Job Title:</span> {user.title}</p>
             <p><span className='card-titles'>Employer:</span> {user.employer}</p>
         </div>
+        <div>
+            <p className='card-titles'>Favorite Movies:</p>
+            <p className='movies'>1. {user.favoriteMovies[0]}</p>
+            <p className='movies'>2. {user.favoriteMovies[1]}</p>
+            <p className='movies'>3. {user.favoriteMovies[2]}</p>
+            
+        </div>
 
-        <p className='card-titles'>Favorite Movies:</p>
-        <p className='movies'>1. {user.favoriteMovies[0]}</p>
-        <p className='movies'>2. {user.favoriteMovies[1]}</p>
-        <p className='movies'>3. {user.favoriteMovies[2]}</p>
         <div className='btn-house'>
             <button className='btn-index' onClick={prev}>{'<'} Previous</button>
             <button className='btn-editors'>Edit</button>
---
+++ src/components/Form.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <form>
+        <p>First name: <input/></p>
+        <p>Last name: <input/></p>
+        <p>City: <input/></p>
+        <p>Country: <input/></p>
+        <p>Job Title: <input/></p>
+        <p>Employer: <input/></p>
+        <h3>Favorite Movies:</h3>
+        <p>1: <input/></p>
+        <p>2: <input/></p>
+        <p>3: <input/></p>
+        <button>Add User</button>
+    </form>
+  )
+}
+
+export default Form
